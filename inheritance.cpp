56486646ced8a4cc95a2cc16ea2fb2bfce6f65f6
nababanalmaidah <nababanalmaidah@gmail.com>
2026-05-11 20:40:50 +0700

    deklarasi member variabel dari class orang

---
+++ inheritance.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//pembuatan class orang
+class orang {
+public:
+    
+    string nama;
+
+    
+    orang(string pNama) : nama(pNama) {
+        cout << "Orang dibuat\n" << endl;
+    }
