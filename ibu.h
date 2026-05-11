f59338ddb9443f96753e0c4bdb2b9547c6ba96b6
nababanalmaidah <nababanalmaidah@gmail.com>
2026-05-11 18:50:49 +0700

    deklarasi prosedur tambah anak dan cetak anak

--- ibu.h
+++ ibu.h
@@ -11,3 +11,31 @@ class anak;
 
 class Ibu {
 public:
+
+    string nama;
+    vector<anak*> daftar_anak;
+
+    Ibu(string pNama) : nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+
+    ~Ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
+
+    void tambahAnak(Anak* pAnak);
+    void cetakAnak();
+};
+
+void Ibu::tambahAnak(Anak* pAnak) {
+    daftar_anak.push_back(pAnak);
+}
+
+void Ibu::cetakAnak() {
+    cout << "Daftar Anak dari Ibu \"" << this->nama << "\":\n";
+    for (auto& anak : daftar_anak) {
+        cout << "- " << anak->nama << endl;
+    }
+}
+
+
