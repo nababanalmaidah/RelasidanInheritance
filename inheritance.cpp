9502f3e396955b91b288f8e70f6bf02622fcde32
nababanalmaidah <nababanalmaidah@gmail.com>
2026-05-11 20:49:47 +0700

    pembuatan class pelajar dan constructor dan descrutor dari class pelajar

--- inheritance.cpp
+++ inheritance.cpp
@@ -24,3 +24,14 @@ class orang {
         return a + b;
     }
 };
+
+class pelajar : public orang {
+public:
+
+    string sekolah;
+
+    
+    pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah) {
+        cout << "Pelajar dibuat\n" << endl;
+    }
+}
