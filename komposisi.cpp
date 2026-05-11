e18c1141f710ca3627a44c465f5338756f9a5968
nababanalmaidah <nababanalmaidah@gmail.com>
2026-05-11 20:36:14 +0700

    deklarasi dan pemberian nilai object pointer varmanusia

--- komposisi.cpp
+++ komposisi.cpp
@@ -5,3 +5,12 @@ using namespace std;
 //menyertakan file header jantung.h dan manusia.h
 #include "jantung.h"
 #include "manusia.h"
+
+int main()
+{
+    //deklarasi dan pemberian nilai object pointer
+    Manusia* varManusia = new Manusia("jono");
+    //mendelete pointer varManusia dari memori
+    delete varManusia;
+    return 0;
+}
