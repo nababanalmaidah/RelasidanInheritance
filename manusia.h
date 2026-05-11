115cfeb159276e1c0c8f46ae730e764a53e50bb5
nababanalmaidah <nababanalmaidah@gmail.com>
2026-05-11 20:21:41 +0700

    deklarasi dan pemberian nilai object pointer varmanusia

--- manusia.h
+++ manusia.h
@@ -1,1 +1,18 @@
-#ifndef 
+#ifndef MANUSIA_H
+#define MANUSIA_H
+//membuat class manusia di file header manusia.h
+class Manusia {
+public:
+    //membuat member variabel class manusia
+    string name;
+    jantung varJantung;
+
+    //membuat constructor dan destructor class manusia
+    Manusia(string pNama) : name(pNama) {
+        cout << name << " Hidup\n";
+    }
+    ~Manusia() {
+        cout << name << " Mati\n";
+    }
+};
+#endif // MANUSIA_H
