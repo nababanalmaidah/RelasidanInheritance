893860ba84aafff88f2d99124d8f8ab2547844e0
nababanalmaidah <nababanalmaidah@gmail.com>
2026-05-11 19:46:18 +0700

    deklarasi prosedur tambah dokter

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -8,3 +8,16 @@ class pasien {
 public:
     string nama;
     vector<dokter*> daftar_dokter;
+
+    pasien(string pNama) :nama(pNama) {
+        cout<< "Pasien \"" << nama << "\" ada\n";
+    };
+
+    ~pasien() {
+        cout << "Pasien \"" << nama << "\" tidak ada\n";
+    }
+
+    //deklarasi prosedur tambahDokter() dan cetakDokter()
+    void tambahDokter(dokter*);
+    void cetakDokter();
+};
