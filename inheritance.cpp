5a4f0fb0f8f792a148aa2096ac93a987d454ebba
nababanalmaidah <nababanalmaidah@gmail.com>
2026-05-11 20:46:47 +0700

    pembuatan function jumlah

--- inheritance.cpp
+++ inheritance.cpp
@@ -12,3 +12,15 @@ public:
     orang(string pNama) : nama(pNama) {
         cout << "Orang dibuat\n" << endl;
     }
+
+    ~orang() {
+        cout << "Orang dihapus\n" << endl;
+    }
+};
+
+class orang {
+    //pembuatan function jumlah
+    int jumlah(int a, int b) {
+        return a + b;
+    }
+};
